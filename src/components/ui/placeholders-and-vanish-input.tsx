ff3b1aaaaa22bee58629ea773a0818bda182d165
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-06-23 10:17:46 +0530

    cheese

--- src/components/ui/placeholders-and-vanish-input.tsx
+++ src/components/ui/placeholders-and-vanish-input.tsx
@@ -193,7 +193,7 @@ export function PlaceholdersAndVanishInput({
       <button
         disabled={!value}
         type="submit"
-        className="absolute right-2 top-1/2 z-50 -translate-y-1/2 h-8 w-8 rounded-full bg-transparent transition duration-200 flex items-center justify-center"
+        className="absolute right-2 top-1/2 z-50 -translate-y-1/2 h-8 w-8 rounded-full disabled:bg-transparent transition bg-orange-400 duration-200 flex items-center justify-center"
       >
         <motion.svg
           xmlns="http://www.w3.org/2000/svg"
